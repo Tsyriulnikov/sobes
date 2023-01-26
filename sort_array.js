02fbd7bf156fce0d193ea1ecf3e93ee193b8d9c5
Tsyriulnikov <max_djvu@mail.ru>
2023-01-26 13:21:40 +0300

    sort array

--- sort_array.js
+++ sort_array.js
@@ -1,9 +1,10 @@
 // const array3_1 = new Array([12,2,3,4,15,6,7,8,9,10,1,5])
 const array3_1 = [12,2,3,4,15,6,7,8,9,10,1,5]
-array3_1.sort((x1, x2)=>{
-    if (x1<x2)  {return -1}
-    if (x1===x2) {return 0}
-    return 1
+// array3_1.sort((x1, x2)=>{
+//     if (x1<x2)  {return -1}
+//     if (x1===x2) {return 0}
+//     return 1
+// })
+array3_1.sort((x1,x2)=>x1-x2)
 
-})
 console.log(array3_1)
