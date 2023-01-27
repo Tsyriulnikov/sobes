4e1c1440e374788f695879812409513c7a9f0929
Tsyriulnikov <max_djvu@mail.ru>
2023-01-27 08:58:15 +0300

    bubble sort

---
+++ bubblesort.js
@@ -0,0 +1,11 @@
+const arrayBubbleSort = [1,9,2,5,3,6]
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.lenght; i++) {
+    for (let j = 0; j < arr.lenght - i - 1; j++) {
+      if (arr[j + 1] < arr[j]) {
+        [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+      }
+    }
+  }
+}
+console.log(bubbleSort(arrayBubbleSort))
