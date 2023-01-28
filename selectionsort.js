7664f90494d626ba0c61ab79437d21c613d2ab73
Tsyriulnikov <max_djvu@mail.ru>
2023-01-28 20:54:14 +0300

    selection sort

---
+++ selectionsort.js
@@ -0,0 +1,12 @@
+//сортировка выборкой
+// const arraySelectSort = [3,5,67,5,8,3,3]
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i 
+    for (let j = i +1; j<arr.length; j++){
+        if(arr[j]<arr[minIndex]) minIndex - j
+
+        
+    }
+  }
+}
