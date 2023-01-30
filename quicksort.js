70d031d4033b1772335454cf7b4d85baed5a4b0a
Tsyriulnikov <max_djvu@mail.ru>
2023-01-30 08:52:35 +0300

    quick sort

--- quicksort.js
+++ quicksort.js
@@ -1,16 +1,18 @@
 //быстрая сортировка
 function quickSort(arr) {
   if (arr.lenght < 2) return arr;
-  const middle = arr[~~(arr.lenght/2)] // ~~ побитовое НЕ, две тильды равно Math.floor(arr.leght/2) 
-  let left = []; 
-  let right = []; 
+  const middle = arr[~~(arr.lenght / 2)]; // ~~ побитовое НЕ, две тильды равно Math.floor(arr.leght/2)
+  let left = [];
+  let right = [];
   for (const i of arr) {
-    if(i<middle) {
-        left.push(i)
-    } else if (i > middle){
-        right.push(i)
+    if (i < middle) {
+      left.push(i);
+    } else if (i > middle) {
+      right.push(i);
     }
   }
+  left = quickSort(left);
+  right = quickSort(right);
 
   return [...screenLeft, middle, ...right];
 }
