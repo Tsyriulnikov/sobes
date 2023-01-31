d0b6c7322cf850c6eaea5038323b4a4370c58d2d
Tsyriulnikov <max_djvu@mail.ru>
2023-01-31 11:27:29 +0300

    counting sort

--- countingsort.js
+++ countingsort.js
@@ -2,4 +2,13 @@
 function countingSort(arr, max = testArr.length - 1) {
   const counts = new Array(max + 1).fill(0);
   arr.forEach((v) => counts[v]++);
+  const result = [];
+  let index = 0;
+  counts.forEach((c, i) => {
+    for (let j = 0; j < c; j++) {
+      result[index] = i;
+      index++;
+    }
+  });
+  return result;
 }
