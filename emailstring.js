e7e1e85e2480b8927d337fc5dfc876b4ea864916
Tsyriulnikov <max_djvu@mail.ru>
2023-02-01 11:03:40 +0300

    cut email

--- emailstring.js
+++ emailstring.js
@@ -1,4 +1,16 @@
-// есть два варианта email 
+// есть два варианта email
 //1. 'Vasay Pertrov <petrov@gmail.com>'
 //2. 'petrov@gmail.com'
 // задача вырезать email
+
+//1 способ
+const str1 = "Vasay Pertrov <petrov@gmail.com>";
+const str2 = "petrov@gmail.com";
+
+const trimEmail = (email) => {
+  let index = email.indexOf("<");
+  if (index !== -1) return email.substring(index + 1, email.length - 1);
+  return email;
+};
+
+console.log(trimEmail(str1));
