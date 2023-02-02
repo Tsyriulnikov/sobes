96d341a6d71596f399df6991065ce716812c44a6
Tsyriulnikov <max_djvu@mail.ru>
2023-02-02 11:28:28 +0300

    es6

--- es6.js
+++ es6.js
@@ -1,1 +1,10 @@
 //определение методов
+const obj = {
+    prop1: function() {return 0},
+    prop2() {return 1},
+    ['prop'+3]() {return 2}
+}
+console.log(obj.prop1());
+console.log(obj.prop2());
+console.log(obj.prop3());
+// 0 1 2
