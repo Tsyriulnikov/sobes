c0e27a47cd606cc394182030c9bf87e1f634b975
Tsyriulnikov <max_djvu@mail.ru>
2023-02-03 13:48:36 +0300

    es6

--- es6.js
+++ es6.js
@@ -1,10 +1,22 @@
 //определение методов
 const obj = {
-    prop1: function() {return 0},
-    prop2() {return 1},
-    ['prop'+3]() {return 2}
-}
+  prop1: function () {
+    return 0;
+  },
+  prop2() {
+    return 1;
+  },
+  ["prop" + 3]() {
+    return 2;
+  },
+};
 console.log(obj.prop1());
 console.log(obj.prop2());
 console.log(obj.prop3());
 // 0 1 2
+
+//создание объекта (create object instance)
+//1
+let object = new Object();
+//2
+object = Object.create(null);
