6169f1b13ac071b40712e04b42e7c3888fe6cfc3
Tsyriulnikov <max_djvu@mail.ru>
2023-02-04 21:13:18 +0300

    binary search

--- binarysearch.js
+++ binarysearch.js
@@ -1,13 +1,19 @@
 function binarySearch(list, item) {
-    let min = 0
-    let max = list.length - 1
-    let steps = 1
-  
-    while (min <= max) {
-      let mid = Math.floor((min + max) / 2)
-  
-      
-      steps++
+  let min = 0;
+  let max = list.length - 1;
+  let steps = 1;
+
+  while (min <= max) {
+    let mid = Math.floor((min + max) / 2);
+
+    if (list[mid] === item) {
+      return `Position: ${mid}, steps: ${steps}`;
+    } else if (list[mid] > item) {
+      max = mid - 1;
+    } else {
+      min = mid + 1;
     }
-    return 'Not found'
+    steps++;
   }
+  return "Not found";
+}
