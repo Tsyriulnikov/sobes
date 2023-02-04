ce453e205f355db4209038f2fbfe7f7b7a6c07d7
Tsyriulnikov <max_djvu@mail.ru>
2023-02-04 21:12:43 +0300

    binary search

---
+++ binarysearch.js
@@ -0,0 +1,13 @@
+function binarySearch(list, item) {
+    let min = 0
+    let max = list.length - 1
+    let steps = 1
+  
+    while (min <= max) {
+      let mid = Math.floor((min + max) / 2)
+  
+      
+      steps++
+    }
+    return 'Not found'
+  }
