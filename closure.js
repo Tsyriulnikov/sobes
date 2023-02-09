16e7d22af9a19e0761a278b7b5d99eb89f6b4be3
Tsyriulnikov <max_djvu@mail.ru>
2023-02-09 14:43:08 +0300

    clouser

---
+++ closure.js
@@ -0,0 +1,12 @@
+function Welcome(name) {
+  var greetingInfo = function (message) {
+    console.log(message + " " + name);
+  };
+  return;
+  greetingInfo;
+}
+var myFunction = Welcome("John");
+myFunction("Welcome ");
+//Output: Welcome John
+myFunction("Hello Mr.");
+//output: Hello Mr.John
