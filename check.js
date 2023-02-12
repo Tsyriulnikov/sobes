7100bcdf3faa563bba5ca8e11498a11abd337134
Tsyriulnikov <max_djvu@mail.ru>
2023-02-12 19:56:28 +0300

    check

---
+++ check.js
@@ -0,0 +1,13 @@
+//check web storage browser support
+if (typeof Storage !== "undefined") {
+  // Code for localStorage/sessionStorage.
+} else {
+  // Sorry! No Web Storage support..
+}
+
+//check web workers browser support
+if (typeof Worker !== "undefined") {
+  // code for Web worker support.
+} else {
+  // Sorry! No Web Worker support..
+}
