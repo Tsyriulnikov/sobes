6e28508eb862d38e68916f163250a3fc0d8e430d
Tsyriulnikov <max_djvu@mail.ru>
2023-02-15 15:40:32 +0300

    bject

--- check.js
+++ check.js
@@ -11,3 +11,9 @@ if (typeof Worker !== "undefined") {
 } else {
   // Sorry! No Web Worker support..
 }
+
+if (typeof EventSource !== "undefined") {
+  // Server-sent events supported. Let's have some code here!
+} else {
+  // No server-sent events supported
+}
